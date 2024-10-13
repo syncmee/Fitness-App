56dc3937198064ea1014557bc1dfda819a6be8ae
syncmee <129711670+syncmee@users.noreply.github.com>
2024-10-13 20:14:17 +0530

    new

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template,url_for
+
+app = Flask(__name__)
+app.secret_key = 'Secret_Key'
+
+
+
+@app.route('/')
+def homepage():
+    return render_template('homepage.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
